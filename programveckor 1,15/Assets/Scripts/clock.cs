58dc67c66ce1826ed487271bea65dd17766450ab
Woody0810 <134690334+Woody0810@users.noreply.github.com>
2025-01-09 21:13:19 +0100

    added clock

--- programveckor 1,15/Assets/Scripts/clock.cs
+++ programveckor 1,15/Assets/Scripts/clock.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class clock : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public TextMeshProUGUI timerText;
+    private float secondsCount;
+    private int minuteCount;
+    private int hourCount;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        UpdateTimerUI();
+    }
+
+    //call this on update
+    public void UpdateTimerUI()
+    {
+        //set timer UI
+        secondsCount += Time.deltaTime;
+        timerText.text = hourCount + "h:" + minuteCount + "m:" + (int)secondsCount + "s";
+        if (secondsCount >= 60)
+        {
+            minuteCount++;
+            secondsCount = 0;
+        }
+        else if (minuteCount >= 60)
+        {
+            hourCount++;
+            minuteCount = 0;
+        }
     }
 }
