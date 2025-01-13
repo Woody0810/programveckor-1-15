34a2d967e237dae5f13ce7b7a4167c576b8bfb5f
Woody0810 <134690334+Woody0810@users.noreply.github.com>
2025-01-13 15:04:32 +0100

    included asprite and more

--- programveckor 1,15/Assets/Scripts/getvaluefromdropdown.cs
+++ programveckor 1,15/Assets/Scripts/getvaluefromdropdown.cs
@@ -1,9 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class getvaluefromdropdown : MonoBehaviour
 {
+    public TMP_Dropdown dropdown;
+    private string dropdownKey = "DropdownSelection";
 
-   
+    void Start()
+    {
+        LoadDropdownSelection();
+        dropdown.onValueChanged.AddListener(delegate { SaveDropdownSelection(); });
+    }
+
+    void SaveDropdownSelection()
+    {
+        PlayerPrefs.SetInt(dropdownKey, dropdown.value);
+
+        PlayerPrefs.Save();
+    }
+    void LoadDropdownSelection()
+    {
+        if (PlayerPrefs.HasKey(dropdownKey))
+        {
+            int savedIndex = PlayerPrefs.GetInt(dropdownKey);
+
+            if (savedIndex >= 0 && savedIndex < dropdown.options.Count)
+            {
+                dropdown.value = savedIndex;
+            }
+        }
+    }
+
 }
