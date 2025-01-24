c76375eee2025d7899889d352f54f8c737e9bbd9
PelleFisk <adrian.o.forsberg@icloud.com>
2025-01-24 09:11:34 +0100

    Changed things in player

---
+++ programveckor 1,15/Assets/Scripts/RespawnOnColliderHit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+public class RespawnOnColliderHit : MonoBehaviour
+{
+	[SerializeField] private Transform respawnPoint;
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.CompareTag("Player"))
+		{
+			other.gameObject.transform.position = respawnPoint.position;
+		}
+	}
+}
